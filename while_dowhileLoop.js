3248c9e8f7879e7cc7b079c14c3e96ada2208ee1
VanshNamdev05 <realvn05@gmail.com>
2025-06-19 18:49:07 +0530

    illustrated the program of doWhile loop

--- while_dowhileLoop.js
+++ while_dowhileLoop.js
@@ -1,3 +1,5 @@
+// While Loop - will execute only if the condition is true
+
 let a = 5;
 let i = 0;
 
@@ -5,3 +7,10 @@ while (i<a) {
     console.log(i);
     i++;
 }
+
+// Do While Loop - will execute atleast once even if the condition is false...
+let num1 = 20;
+let num2 = 20;
+do {
+    console.log(num1)
+} while (num1<num2);
