1dae8825c434dc0166cf502ae8ad1db4536bde58
VanshNamdev05 <realvn05@gmail.com>
2025-06-29 23:55:46 +0530

    parent and sibling element

---
+++ parents_and_siblings/script.js
@@ -0,0 +1,8 @@
+// DOM collections are read only
+// They are iterable using for... of loop
+
+// siblings -> nodes that are children of the same parent 
+// head and body are siblings...
+
+console.log(document.body.childNodes[1].parentNode);
+
