2dac0e342b22c1e9a63ebd463bc5496a658fc36c
VanshNamdev05 <realvn05@gmail.com>
2025-07-05 23:00:52 +0530

    practice_toggle_classlist_events

---
+++ practice_chapter_8/script.js
@@ -0,0 +1,7 @@
+// document.getElementById("google").addEventListener("click", () => {
+//     window.open("https://google.com", "_blank");
+// });
+
+setInterval(() => {
+    document.querySelector("#bulb").classList.toggle("bulb");
+}, 300);
