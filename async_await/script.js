9bec71f6d9c73cbafd0a7b20b19f998109624d32
VanshNamdev05 <realvn05@gmail.com>
2025-07-14 22:40:14 +0530

    using async and await

--- async_await/script.js
+++ async_await/script.js
@@ -1,11 +1,43 @@
 // async function always return a promise and we can use .then()... and so on
 
 async function vansh(params) {
-    return 5;
+    let delhiWeather = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("27 Deg")
+        }, 1000);
+    })
+
+    let bengaloreWeather = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("21 Deg")
+        }, 2000);
+    })
+
+    // delhiWeather.then(alert);
+    // bengaloreWeather.then(alert);
+
+    console.log("Fetching delhi Weather... Please Wait.....")
+    let delhiW = await delhiWeather;
+    console.log("Delhi Weather is fetched... : " + delhiW)
+
+    console.log("Fetching bengalore Weather... Please Wait.....")
+    let bengW = await bengaloreWeather;
+    console.log("Bengalore Weather is fetched... : " + bengW)
+
+    return [delhiW, bengW];
+
 }
 
-console.log(vansh);
+const cherry = async () => {
+    console.log("Hey i am cherry and i am waiting...");
+}
 
-vansh().then((x)=>{
-    alert(x);
-})
+console.log("Welcome to weather control room")
+
+const main = async () => {
+
+    let a = await vansh();
+    let b = await cherry();
+}
+
+main();
