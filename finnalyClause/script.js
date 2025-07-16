15c485707620d5ccb0967010b35c3b36c7c9ed0c
VanshNamdev05 <realvn05@gmail.com>
2025-07-16 22:33:36 +0530

    finally clause

---
+++ finnalyClause/script.js
@@ -0,0 +1,16 @@
+const f = () => {
+
+    try {
+        let a = 0;
+        nalio = uore;
+        console.log("Program ran successfully");
+    } catch (error) {
+        console.log("This is an error");
+        console.log(p);
+    } finally {
+        console.log("I am a good boy...")
+    }
+
+}
+f();
+console.log("HELLO...");
